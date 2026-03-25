ad0e6fc88b5439d2029b73ba8ecde587f34cec06
Alfredo C <alfredo_cardenas@live.com>
2026-03-24 21:07:28 -0600

    feat: setup and add the AuthenticationController - which will have a Post method for Login - and which will need 1 Service class, and 2 dtos - the AuthenticationService has 2 methods: authenticate and generateToken - and the Dtos LoginRequest and AuthResponse, are used by the controller, to serve as DataStructures for the login-request & login-response data

---
+++ src/main/java/com/jalfreddev/blog/domain/dtos/AuthResponse.java
@@ -0,0 +1,17 @@
+package com.jalfreddev.blog.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthResponse {
+
+  private String token;  //JWT value
+  private Long expiresIn;  //just to have it available easily, it's inside the token itself
+
+}
