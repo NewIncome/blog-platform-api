f8c640fd382cc7c91e050859d7839cbafa1ca4f4
Alfredo C <alfredo_cardenas@live.com>
2026-04-29 15:22:52 -0600

    feat: use the JwtAuthFilter in the SecurityConfig - add beans for the JwtAuthenticationFilter, and for the UserDetailsService - then in the HttpSecurity object add the requestMatcher for a POST request to api/vi/auth - at last, to the same object use the addFilterBefore method to add the JwtAuthFilter bean before the UsernamePasswordAuthFilter class

--- src/main/java/com/jalfreddev/blog/config/SecurityConfig.java
+++ src/main/java/com/jalfreddev/blog/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.jalfreddev.blog.config;
 
+import com.jalfreddev.blog.repositories.UserRepository;
+import com.jalfreddev.blog.security.BlogUserDetailsService;
+import com.jalfreddev.blog.security.JwtAuthenticationFilter;
+import com.jalfreddev.blog.services.AuthenticationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,27 +11,45 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  public JwtAuthenticationFilter jwtAuthenticationFilter(AuthenticationService authenticationService) {
+    return new JwtAuthenticationFilter(authenticationService);
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService(UserRepository userRepository) {
+    return new BlogUserDetailsService(userRepository);
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(
+                                HttpSecurity http,
+                                JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
     //To specify security filters. Like for the app's EndPoints, for CSRF,and SessionManagement
     http
         .authorizeHttpRequests(auth -> auth
+            .requestMatchers(HttpMethod.POST, "/api/v1/auth").permitAll()
             .requestMatchers(HttpMethod.GET, "/api/v1/posts/**").permitAll()
             .requestMatchers(HttpMethod.GET, "/api/v1/categories/**").permitAll()
             .requestMatchers(HttpMethod.GET, "/api/v1/tags/**").permitAll()
             .anyRequest().authenticated()
+            //To make a quick test to see if there's a problem in the SecConfig here do only:
+            // .anyRequest().permitAll()
         )
         .csrf(csrf -> csrf.disable())
         .sessionManagement(session ->
             session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        );
+          //this adds the jwtAuthFilter before the UserPassAuthFilter
+        ).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
